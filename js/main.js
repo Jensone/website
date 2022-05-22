7399202b8e93bb459d0a695bb0f6e5db254047d5
Jensone <jeancharlesjensone@gmail.com>
2022-05-22 21:46:41 +0200

    Markdown converter setting up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 // Variables
-const menu_icon = document.querySelector('.menu_icon');
-const menu_icon_close = document.querySelector('.menu_icon_close');
-const menu = document.querySelector('.menu');
+const menu_icon = document.querySelector('.menu_icon')
+const menu_icon_close = document.querySelector('.menu_icon_close')
+const menu = document.querySelector('.menu')
+const md2html = document.querySelector('section')
 
 // Menu active
 menu_icon.addEventListener('click', () => {
-    menu.classList.add('active');
+    menu.classList.add('active')
 })
 menu_icon_close.addEventListener('click', () => {
-    menu.classList.remove('active');
+    menu.classList.remove('active')
 })
 
+// Title page setting
 document.querySelector('h1').innerHTML = document.title
+
+// Last update
 document.querySelector('.last_edit').innerHTML = document.lastModified
 
 
